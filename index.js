bbdcbf696f4f735f4471fc974cf44dc5c4eb4912
DESKTOP-571MKVN\seclab <longlngs@naver.com>
2023-06-07 21:37:49 +0900

    intro 완성

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
+/* variables */
 var video = document.getElementById('intro-video');
 var playButton = document.getElementById('play-button');
 var intro = document.getElementById('intro');
+var background = document.getElementById('background');
+var startButton = document.querySelector('.start-button');
 var introChildren = intro.children;
 
+/* before video starting */
 intro.classList.add('hide');
+background.classList.add('hide');
 
 for (var i = 0; i < introChildren.length; i++) {
     introChildren[i].classList.add('hide'); 
@@ -14,11 +19,28 @@ playButton.addEventListener('click', function() {
     this.style.display = 'none';
 });
 
+/* after the video is ended */
+
 video.addEventListener('ended', function() {
     this.style.display = 'none';
+    background.classList.remove('hide');
     intro.classList.remove('hide');
+
+    background.style.animation = 'fadeIn 2s'; 
+    intro.style.animation = 'fadeIn 2s'; 
 
     for (var i = 0; i < introChildren.length; i++) {
         introChildren[i].classList.remove('hide'); 
+        introChildren[i].style.animation = 'fadeIn 2s';
     }
+});
+
+startButton.addEventListener('click', function() {    
+    intro.style.animation = 'fadeOut 2s'; 
+    background.style.animation = 'fadeOut 2s';
+
+    intro.addEventListener('animationend', function() {
+        
+        window.location.href = 'next_page.html';
+    });
 });

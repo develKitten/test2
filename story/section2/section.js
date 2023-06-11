81080c3aa9903fd36990429277c1c27a16759103
DESKTOP-571MKVN\seclab <longlngs@naver.com>
2023-06-12 04:59:54 +0900

    1탄완성

--- story/section2/section.js
+++ story/section2/section.js
@@ -1,6 +1,19 @@
-$(function () {
-    $(document).scroll(function () {
-        var $nav = $(".navbar");
-        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-    });
+/* 헤더 기능 */
+document.querySelector("nav").classList.add("fixed-top");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 180 || document.documentElement.scrollTop > 180) {
+    document.querySelector("nav").classList.add("fixed-top");
+  } else {
+    document.querySelector("nav").classList.remove("fixed-top");
+  }
+}
+
+window.addEventListener("load", scrollFunction);
+
+/* 슬라이드 관련 */
+$(document).ready(function() {
+  $('#myCarousel').carousel();
 });
